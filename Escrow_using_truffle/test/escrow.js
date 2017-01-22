50bf1d0bcfb3445ce5ab1e56fd1af2d31fd10787
Axel Hodler <derxor@gmail.com>
2017-01-22 09:58:27 +0100

    escrow accepts ether

--- Escrow_using_truffle/test/escrow.js
+++ Escrow_using_truffle/test/escrow.js
@@ -1,2 +1,12 @@
 contract('Escrow', (accounts) => {
+  it('accepts ether', (done) => {
+    return web3.eth.sendTransaction({
+      from: accounts[0],
+      to: Escrow.deployed().address,
+      value: web3.toWei(1, 'ether')
+    }, () => {
+      assert.equal(web3.fromWei(web3.eth.getBalance(Escrow.deployed().address).toNumber()), 1);
+      done();
+    })
+  });
 });
