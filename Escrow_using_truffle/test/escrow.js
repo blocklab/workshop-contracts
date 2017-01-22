fab7c8102361ab4ea3538451bb6c198237974e51
Axel Hodler <derxor@gmail.com>
2017-01-22 10:19:28 +0100

    extract to helper functions

--- Escrow_using_truffle/test/escrow.js
+++ Escrow_using_truffle/test/escrow.js
@@ -3,27 +3,31 @@ contract('Escrow', (accounts) => {
   const bob = accounts[1];
   let contract;
 
+  let balanceInEther = (account) => {
+    return +web3.fromWei(web3.eth.getBalance(account).toNumber());
+  };
+
   beforeEach(() => {
     contract = Escrow.deployed();
   });
 
   it('accepts ether', (done) => {
     return web3.eth.sendTransaction({
-      from: accounts[0],
+      from: alice,
       to: contract.address,
       value: web3.toWei(1, 'ether')
     }, () => {
-      assert.equal(web3.fromWei(web3.eth.getBalance(contract.address).toNumber()), 1);
+      assert.equal(balanceInEther(contract.address), 1);
       done();
     })
   });
 
   it('can be finalized by the buyer', () => {
-    const bobsInitialBalance = web3.fromWei(web3.eth.getBalance(bob).toNumber());
+    const bobsInitialBalance = balanceInEther(bob);
     return contract.paySeller({
-      from: accounts[0]
+      from: alice,
     }).then(() => {
-      assert.equal(+web3.fromWei(web3.eth.getBalance(accounts[1]).toNumber()), +bobsInitialBalance + 1)
+      assert.equal(balanceInEther(bob), bobsInitialBalance + 1)
     });
   });
 });
