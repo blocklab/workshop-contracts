d4cd72fb18a3851ab213954605081f4399d9306e
Axel Hodler <derxor@gmail.com>
2017-01-22 11:02:23 +0100

    add refund functionality

--- Escrow_using_truffle/contracts/Escrow.sol
+++ Escrow_using_truffle/contracts/Escrow.sol
@@ -3,10 +3,12 @@ pragma solidity ^0.4.2;
 contract Escrow {
   address seller;
   address buyer;
+  address moderator;
 
-  function Escrow(address _seller) {
+  function Escrow(address _seller, address _moderator) {
     seller = _seller;
     buyer = msg.sender;
+    moderator = _moderator;
   }
 
   function() payable {
@@ -15,5 +17,10 @@ contract Escrow {
   function paySeller() {
     if (msg.sender != buyer) throw;
     if (!seller.send(this.balance)) throw;
+  }
+
+  function refundBuyer() {
+    if (msg.sender == seller || msg.sender == moderator)
+      if (!buyer.send(this.balance)) throw;
   }
 }
