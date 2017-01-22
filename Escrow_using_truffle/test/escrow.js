4678eb5b4336296e246d0891cd09b739a4ea4497
Axel Hodler <derxor@gmail.com>
2017-01-22 10:00:27 +0100

    improve structure

--- Escrow_using_truffle/test/escrow.js
+++ Escrow_using_truffle/test/escrow.js
@@ -1,11 +1,18 @@
 contract('Escrow', (accounts) => {
+  const alice = accounts[0];
+  let contract;
+
+  beforeEach(() => {
+    contract = Escrow.deployed();
+  });
+
   it('accepts ether', (done) => {
     return web3.eth.sendTransaction({
       from: accounts[0],
-      to: Escrow.deployed().address,
+      to: contract.address,
       value: web3.toWei(1, 'ether')
     }, () => {
-      assert.equal(web3.fromWei(web3.eth.getBalance(Escrow.deployed().address).toNumber()), 1);
+      assert.equal(web3.fromWei(web3.eth.getBalance(contract.address).toNumber()), 1);
       done();
     })
   });
