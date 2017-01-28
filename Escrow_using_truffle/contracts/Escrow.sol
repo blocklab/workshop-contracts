22792e79b7c49a0b4c5fd6d1ad88d92ebde10f6c
Axel Hodler <derxor@gmail.com>
2017-01-28 09:11:16 +0100

    finalize escrow with tests

--- Escrow_using_truffle/contracts/Escrow.sol
+++ Escrow_using_truffle/contracts/Escrow.sol
@@ -5,18 +5,18 @@ contract Escrow {
   address buyer;
   address moderator;
 
-  function Escrow(address _seller, address _moderator) {
+  function Escrow(address _seller, address _buyer, address _moderator) {
     seller = _seller;
-    buyer = msg.sender;
+    buyer = _buyer;
     moderator = _moderator;
   }
 
-  function() payable {
+  function fund() payable {
   }
 
   function paySeller() {
-    if (msg.sender != buyer) throw;
-    if (!seller.send(this.balance)) throw;
+    if (msg.sender == buyer || msg.sender == moderator)
+      if (!seller.send(this.balance)) throw;
   }
 
   function refundBuyer() {
--- Escrow_using_truffle/test/escrow.js
+++ Escrow_using_truffle/test/escrow.js
@@ -1,45 +1,107 @@
-contract('Escrow', (accounts) => {
-  const aliceTheBuyer = accounts[0];
-  const bobTheSeller = accounts[1];
-  let contract;
+const FUNDING_AMOUNT = 1;
 
-  let balanceInEther = (account) => {
-    return +web3.fromWei(web3.eth.getBalance(account).toNumber());
-  };
+let balanceInEther = (account) => {
+  return +web3.fromWei(web3.eth.getBalance(account).toNumber());
+};
 
-  beforeEach(() => {
-    contract = Escrow.deployed();
-  });
+let provideFunding = (contract, buyer) => {
+  return contract.fund.sendTransaction({
+    from: buyer,
+    to: contract.address,
+    value: web3.toWei(FUNDING_AMOUNT, 'ether')
+  })
+};
 
-  it('accepts ether', (done) => {
-    return web3.eth.sendTransaction({
-      from: aliceTheBuyer,
-      to: contract.address,
-      value: web3.toWei(1, 'ether')
-    }, () => {
-      assert.equal(balanceInEther(contract.address), 1);
-      done();
-    })
-  });
+contract('Escrow - Buyer releases funds', (accounts) => {
+  const buyer = accounts[0];
+  const seller = accounts[1];
 
   it('can be finalized by the buyer', () => {
-    const bobsInitialBalance = balanceInEther(bobTheSeller);
-    return contract.paySeller({
-      from: aliceTheBuyer,
-    }).then(() => {
-      assert.equal(balanceInEther(bobTheSeller), bobsInitialBalance + 1)
-    });
-  });
+    let contract = Escrow.deployed();
+    return provideFunding(contract, buyer)
+      .then(() => {
+        const sellerBalanceBeforeBeingPaid = balanceInEther(seller);
+        return contract.paySeller({
+          from: buyer,
+        }).then(() => {
+          assert.equal(balanceInEther(seller), sellerBalanceBeforeBeingPaid + FUNDING_AMOUNT);
+        });
+      })
+  })
+});
 
-  it('cant be finalized by the seller', (done) => {
-    contract.paySeller({
-      from: bobTheSeller
-    }).catch(() => {
-      assert.ok(true);
-      done();
-    }).then(() => {
-      assert.fail();
-      done();
-    })
+contract('Escrow - Seller attacks', (accounts) => {
+  const buyer = accounts[0];
+  const seller = accounts[1];
+
+  it('does not allow seller to pay himself with the escrow money', () => {
+    let contract = Escrow.deployed();
+    return provideFunding(contract, buyer)
+      .then(() => {
+        const sellerBalanceBeforeAttemptingToStealFunds = balanceInEther(seller);
+        return contract.paySeller({
+          from: seller
+        }).then(() => {
+          assert.isAtMost(balanceInEther(seller), sellerBalanceBeforeAttemptingToStealFunds);
+        });
+      })
+  });
+});
+
+contract('Escrow - Something with shipping went wrong', (accounts) => {
+  const buyer = accounts[0];
+  const seller = accounts[1];
+
+  it('allows seller to release funds to buyer', () => {
+    let contract = Escrow.deployed();
+    return provideFunding(contract, buyer)
+      .then(() => {
+          const buyerBalanceAfterFunding = balanceInEther(buyer);
+          return contract.refundBuyer({
+            from: seller,
+          }).then(() => {
+            assert.equal(balanceInEther(buyer), buyerBalanceAfterFunding + FUNDING_AMOUNT);
+          });
+        }
+      );
+  });
+});
+
+contract('Escrow - Conflict case - Buyer is to blame', (accounts) => {
+  const buyer = accounts[0];
+  const seller = accounts[1];
+  const moderator = accounts[2];
+
+  it('allows moderator to release funds to seller', () => {
+    let contract = Escrow.deployed();
+    return provideFunding(contract, buyer)
+      .then(() => {
+          const buyerBalanceBeforeFunding = balanceInEther(seller);
+          return contract.paySeller({
+            from: moderator,
+          }).then(() => {
+            assert.equal(balanceInEther(seller), buyerBalanceBeforeFunding + FUNDING_AMOUNT);
+          });
+        }
+      );
+  });
+});
+
+contract('Escrow - Conflict case - Seller is to blame', (accounts) => {
+  const buyer = accounts[0];
+  const moderator = accounts[2];
+
+  it('allows moderator to release funds back to buyer', () => {
+    let contract = Escrow.deployed();
+    return provideFunding(contract, buyer)
+      .then(() => {
+          const buyerBalanceAfterFunding = balanceInEther(buyer);
+          return contract.refundBuyer({
+            from: moderator,
+          }).then(() => {
+            assert.equal(balanceInEther(buyer), buyerBalanceAfterFunding + FUNDING_AMOUNT);
+          });
+        }
+      );
   });
 });
