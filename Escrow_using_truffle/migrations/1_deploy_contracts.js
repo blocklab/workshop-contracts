f2468576cb0487b8cc3fbb46038430846c41e371
Axel Hodler <derxor@gmail.com>
2017-01-28 09:11:00 +0100

    remove unnecessary stuff

---
+++ Escrow_using_truffle/migrations/1_deploy_contracts.js
@@ -0,0 +1,3 @@
+module.exports = function(deployer) {
+  deployer.deploy(Escrow, '0xdf700fd0413ca5772cbf5a588d3080469f2edda2', '0x38588822bea476d5e1d56cfc9ce9781fe5262196', '0x354990de9386772900c6b257ec0b8dcc5af8bfba');
+};
--- Escrow_using_truffle/migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.autolink();
-  deployer.deploy(Escrow, '0xdf700fd0413ca5772cbf5a588d3080469f2edda2', '0x354990de9386772900c6b257ec0b8dcc5af8bfba');
-};
